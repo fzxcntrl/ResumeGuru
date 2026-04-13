bb5b83092f7de843316123c60e64a28a8eb7ff32
fzxcntrl <farzainnaikwade@gmail.com>
2026-04-13 20:17:02 +0530

    .

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path');
 
 // Load environment variables
 dotenv.config();
@@ -31,14 +30,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Port setting
 const PORT = process.env.PORT || 5000;
-
-// Serve static build from frontend
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// Fallback route for SPA - needs to be AFTER APIs and static files
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
